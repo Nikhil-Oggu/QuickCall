df704903c5b0c6a5df35a61ac249ed2451b3a47f
Nikhil Oggu <oggunikhil174@gmail.com>
2026-01-08 22:06:07 +0530

    Update server URL to use production endpoint

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,3 +1,4 @@
-const server = process.env.REACT_APP_SERVER_URL || "http://localhost:8000";
+// const server = process.env.REACT_APP_SERVER_URL || "http://localhost:8000";
+const server = "https://zoomclonebackend-78u8.onrender.com/";
 
 export default server;
